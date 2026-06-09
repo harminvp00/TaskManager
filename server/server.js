98ce619e3bf2221050fc01c96d0ec178e3a06eeb
ashish-v-ai <ashishvekariya165@gmail.com>
2026-06-09 22:48:41 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,4 @@
-
 import "dotenv/config";
-import createHttpError from "http-errors";
 import connectDB from "./src/config/db.js"
 import express from "express";
 const app = express();
@@ -9,19 +7,6 @@ import userRoutes from "./src/modules/auth/auth.routes.js"
 connectDB();
 app.use(express.json());
 app.use("/accounts", userRoutes);
-
-app.get("/",(req,res) => {
-    const newerr = createHttpError(500,"Internal server eeeor");
-    return next(newerr);
-})
-
-app.use((err,req,res,next) =>{
-    const statusCode = err.statusCode || 500;
-
-    return res.status(statusCode).json({
-        message : err.message
-    })
-});
 
 // open server to listen requests
 app.listen(process.env.PORT,() =>{
