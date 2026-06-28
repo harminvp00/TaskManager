21282bda4ce6ef1b17a519db53509eee37177b6e
ashish-v-ai <ashishvekariya165@gmail.com>
2026-06-28 16:49:58 +0530

    add some token and hash funtions

--- server/src/utils/tokens/token.js
+++ server/src/utils/tokens/token.js
@@ -1,15 +1,31 @@
-
 import jwt, { decode } from "jsonwebtoken";
 import "dotenv/config";
+import crypto from "crypto";
 
-export const getToken = (id, email, role) => {
+export const getAccessToken = (id, email, role, sessionId) => {
   const token = jwt.sign(
     {
       id: id,
       email: email,
       role: role,
+      sessionId,
     },
     process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "10m",
+    },
+  );
+  return token;
+};
+export const getRefreshToken = (id, email, role) => {
+  const token = jwt.sign(
+    {
+      id: id,
+      email: email,
+      role: role,
+      jti: crypto.randomUUID(),
+    },
+    process.env.JWT_REFRESH_SECRET_KEY,
     {
       expiresIn: "7d",
     },
@@ -17,14 +33,26 @@ export const getToken = (id, email, role) => {
   return token;
 };
 
-
-
-export const decodeToken =  (token) =>{
-
+export const decodeAccessToken = (token) => {
   const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
   return {
     id: decode.id,
-    email: decode.email
-  }
+    email: decode.email,
+  };
+};
+
+export const decodeRefreshToken = (token) => {
+  const decode = jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY);
+
+  return {
+    id: decode.id,
+    email: decode.email,
+    sessionId: decode.sessionId,
+  };
+};
+
+export const hashToken = async (token) => {
+  const hashedToken = await crypto.createHash("sha256").update(token).digest("hex");
+  return hashedToken;
 }
