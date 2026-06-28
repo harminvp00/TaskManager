868bfba292abedc578ef15c311aa64739a752f99
ashish-v-ai <ashishvekariya165@gmail.com>
2026-06-28 15:56:59 +0530

    add session model

---
+++ server/src/modules/session/session.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    ipAddress: {
+      type: String,
+    },
+    userAgent: {
+      type: String,
+    },
+    revoked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+const Session = mongoose.model("Session", sessionSchema);
+
+export default Session;
