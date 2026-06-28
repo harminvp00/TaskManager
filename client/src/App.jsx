0f667dc305f84ccdb3c8ddc91b0e0661d7560955
Harmin Vekariya <harminv251@gmail.com>
2026-06-29 00:52:13 +0530

    Login Panel and Dashboard Add to test the Github and Google OAuth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,18 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from "./pages/Login"
+import Dashboard from './pages/Dashboard';
 
 function App() {
-
-  const styles =  {
-    display: 'flex', 
-    height: '100vh', 
-    width: '100%', 
-    justifyContent:'center', 
-    alignItems:'center'
-  }
-
-  function loginUsingGit(){
-    window.location.href = "http://localhost:3000/gitAuth/login"
-  }
-
   return (
-    <div style={styles}>
-      <button
-      onClick={()=> loginUsingGit()}>
-          login using github
-      </button>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
